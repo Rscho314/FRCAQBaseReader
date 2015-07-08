1c6717ac2386e992c0b9d991939dd96f7ee21246
Raoul Schorer <raoul.schorer@gmail.com>
2015-07-08 22:31:50 +0200

    Mark dialog with buttons

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +12,7 @@ import javax.swing.border.EmptyBorder;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Random;
@@ -28,7 +31,12 @@ public class MainWindow extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	private final int examSize = 5;
+	private int questionsDone = 1;
+	private String[][] gaa = new String[examSize][5]; //given answers array
+	private int[] raa = new int[examSize]; //registered answers array
+	public static float mark;
+	public static MainWindow frame;
 	/**
 	 * Launch the application.
 	 */
@@ -36,7 +44,7 @@ public class MainWindow extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainWindow frame = new MainWindow();
+					frame = new MainWindow();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -53,49 +61,96 @@ public class MainWindow extends JFrame {
 	public MainWindow() throws URISyntaxException, Exception {
 		
 		//Logic
-		Logic L = new Logic();
+		final Logic L = new Logic();
+		
 		File f = L.ChooseQuestion();
-		String[][] qa = L.ReadQuestion(f);
-		//System.out.println(Arrays.deepToString(qa));
+		String[][] qa = L.ReadQuestion(f); //question parts array
+		String[] aa = {qa[1][0], qa[2][0], qa[3][0], qa[4][0], qa[5][0]}; //answers array
+		//System.out.println(Arrays.deepToString(aa));
 		
 		//GUI
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 350);
+		setLocationRelativeTo(null); //center window on screen
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblQuestion = new JLabel("<html>"+qa[0][0]+"</html>");
-		lblQuestion.setBorder(new EmptyBorder(10,10,20,0));
-		contentPane.add(lblQuestion, BorderLayout.NORTH);
-		
-		JButton btnValidate = new JButton("validate");
-		contentPane.add(btnValidate, BorderLayout.SOUTH);
-		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
-		JCheckBox chckbxAns = new JCheckBox(qa[1][1]);
+		final JLabel lblQuestion = new JLabel("<html>"+qa[0][0]+"</html>");
+		lblQuestion.setBorder(new EmptyBorder(10,10,20,0));
+		panel.add(lblQuestion);
+		
+		final JCheckBox chckbxAns = new JCheckBox("<html>"+qa[1][1]+"</html>");
 		chckbxAns.setBorder(new EmptyBorder(0,15,10,0));
 		panel.add(chckbxAns);
 		
-		JCheckBox chckbxAns_1 = new JCheckBox(qa[2][1]);
+		final JCheckBox chckbxAns_1 = new JCheckBox("<html>"+qa[2][1]+"</html>");
 		chckbxAns_1.setBorder(new EmptyBorder(0,15,10,0));
 		panel.add(chckbxAns_1);
 		
-		JCheckBox chckbxAns_2 = new JCheckBox(qa[3][1]);
+		final JCheckBox chckbxAns_2 = new JCheckBox("<html>"+qa[3][1]+"</html>");
 		chckbxAns_2.setBorder(new EmptyBorder(0,15,10,0));
 		panel.add(chckbxAns_2);
 		
-		JCheckBox chckbxAns_3 = new JCheckBox(qa[4][1]);
+		final JCheckBox chckbxAns_3 = new JCheckBox("<html>"+qa[4][1]+"</html>");
 		chckbxAns_3.setBorder(new EmptyBorder(0,15,10,0));
 		panel.add(chckbxAns_3);
 		
-		JCheckBox chckbxAns_4 = new JCheckBox(qa[5][1]);
+		final JCheckBox chckbxAns_4 = new JCheckBox("<html>"+qa[5][1]+"</html>");
 		chckbxAns_4.setBorder(new EmptyBorder(0,15,10,0));
 		panel.add(chckbxAns_4);
+		
+		final JButton btnValidate = new JButton("validate");
+		btnValidate.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				File f;
+				try {
+					if(questionsDone < examSize){
+						f = L.ChooseQuestion();
+						String[][] qa = L.ReadQuestion(f);
+						String[] aa = {qa[1][0], qa[2][0], qa[3][0], qa[4][0], qa[5][0]};
+						lblQuestion.setText("<html>"+qa[0][0]+"</html>");
+						chckbxAns.setText("<html>"+qa[1][1]+"</html>");
+						chckbxAns_1.setText("<html>"+qa[2][1]+"</html>");
+						chckbxAns_2.setText("<html>"+qa[3][1]+"</html>");
+						chckbxAns_3.setText("<html>"+qa[4][1]+"</html>");
+						chckbxAns_4.setText("<html>"+qa[5][1]+"</html>");
+						
+						if(chckbxAns.isSelected()){gaa[questionsDone][0] = "true";}else{gaa[questionsDone][0] = "false";}
+						if(chckbxAns_1.isSelected()){gaa[questionsDone][0] = "true";}else{gaa[questionsDone][1] = "false";}
+						if(chckbxAns_2.isSelected()){gaa[questionsDone][0] = "true";}else{gaa[questionsDone][2] = "false";}
+						if(chckbxAns_3.isSelected()){gaa[questionsDone][0] = "true";}else{gaa[questionsDone][3] = "false";}
+						if(chckbxAns_4.isSelected()){gaa[questionsDone][0] = "true";}else{gaa[questionsDone][4] = "false";}
+						
+						if(gaa[questionsDone] == aa){raa[questionsDone] = 1;}else{raa[questionsDone] = 0;}
+						
+						questionsDone = ++questionsDone;
+					}else{
+						System.out.println(Arrays.toString(raa));
+						for (int i : raa)
+						    mark += i;
+						mark = mark*100/examSize; // exam mark in percents
+						MainWindow.this.setVisible(false); //hide the questions window
+						ReviewDialog rd = new ReviewDialog();
+						rd.setVisible(true);
+					}
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+			}
+			
+		});
+		contentPane.add(btnValidate, BorderLayout.SOUTH);
 
 		this.setTitle("FRCA QBase Reader");
 		
---
+++ src/main/ReviewDialog.java
@@ -0,0 +1,89 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class ReviewDialog extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ReviewDialog(){
+		setBounds(100, 100, 500, 350);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+
+	        @Override
+	        public void windowClosing(WindowEvent arg0) {
+	             MainWindow.frame.setVisible(true);
+	        }
+
+
+
+	    });
+		
+		JPanel panel = new JPanel();
+		setContentPane(panel);
+		//panel.setLayout(new BorderLayout(panel, BorderLayout.CENTER));
+		
+		JPanel txtPanel = new JPanel();
+		txtPanel.setBorder(new EmptyBorder(5, 5, 50, 5));
+		txtPanel.setLayout(new BoxLayout(txtPanel, BoxLayout.Y_AXIS));
+		panel.add(txtPanel);
+		
+		JLabel lblAppreciation = new JLabel("", SwingConstants.CENTER);
+		if(MainWindow.mark >= 0.8){
+			lblAppreciation.setText("<html>You scored <font color='green'>"+ MainWindow.mark + "%</font>!</html>");
+		}else{
+			lblAppreciation.setText("<html>You scored <font color='red' size='6'>"+ MainWindow.mark + "%</font>!</html>");
+		}
+		txtPanel.add(lblAppreciation);
+		
+		JLabel lblRename = new JLabel("", SwingConstants.CENTER);
+		if(MainWindow.mark >= 0.8){
+			lblRename.setText("<html>You will likely pass a lot of gas until retirement.</html>");
+		}else{
+			lblRename.setText("<html>Keep trying, you type A!</html>");
+		}
+		txtPanel.add(lblRename);
+		
+		JPanel btnPanel = new JPanel();
+		btnPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.ipadx = 20;
+        gbc.ipady = 20;
+
+		panel.add(btnPanel);
+		
+		JButton btnReview = new JButton("Review wrong answers");
+		btnPanel.add(btnReview, gbc);
+		gbc.gridy++;
+		
+		JButton btnRestart = new JButton("Start a new session");
+		btnPanel.add(btnRestart, gbc);
+		gbc.gridy++;
+		
+		JButton btnQuit = new JButton("Quit");
+		btnPanel.add(btnQuit, gbc);
+		gbc.gridy++;
+	}
+
+}
